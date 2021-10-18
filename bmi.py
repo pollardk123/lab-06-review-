b0126dab957f6604191e9ace58e699f0a07bacbe
pollardk123 <pollark836@gmail.com>
2021-10-18 17:18:35 -0400

    created BMI.py

---
+++ bmi.py
@@ -0,0 +1,10 @@
+import sys
+print( " BMI : Body Mass Index Calculator v.0.1 beta")
+userWeight = input("Enter your weight ( in pounds): ")
+userHeight = input(" Enter your height (in inches): ")
+
+# myBMI = (703 * float(userWeight)) / (float(userHeight) * float(userHeight))
+myBMI = (703 * float(userWeight))  / pow(float(userHeight), 2)
+
+myBMI = round(myBMI, 2)
+print("Your body mass index (BMI) is " + str(myBMI) + "%" )
